3064c936a96a6f6b2a9b5fe976e7691bfd49c97b
Sahil Sinha <123497138+Ithurtzwhenip@users.noreply.github.com>
2024-05-04 20:32:57 +0530

    Showing a spinner

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,11 @@
 import useGenres, { Genre } from "../hooks/useGenre";
-import {HStack, Image, List, ListItem, Text} from "@chakra-ui/react";
+import {HStack, Image, List, ListItem, Spinner, Text} from "@chakra-ui/react";
 import React from "react";
 
 const GenreList = ()=>{
-    const {data} = useGenres();
+    const {data,isLoading,error} = useGenres();
+    if(error) return null;
+    if (isLoading) return  <Spinner/>;
     return(
         <List>
             {data.map(genre=>
